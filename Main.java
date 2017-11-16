5fb1780adaa16d8444e0f4e45e511620c93ad6f6
Joakim Konac <joako217@student.liu.se>
2017-11-16 19:54:03 +0100

    Add files

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    ImageGenerator initiateProgram = new ImageGenerator();
+        try {
+            initiateProgram.Generate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
